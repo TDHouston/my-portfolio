2ffcb145eb22154ab177de86efbbc92719fd7003
TDHouston <trentdhouston@gmail.com>
2024-05-13 21:51:43 -0400

    added web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Nav";
@@ -23,6 +24,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <Analytics />
     </html>
   );
 }
